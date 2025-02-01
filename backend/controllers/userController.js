c54c7f36a17d9ae64c1ae597c54d8fad7ac70300
Viktoria <toz.vito@gmail.com>
2025-02-01 17:18:27 +0100

    Adds jwt token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,14 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const {User} = require('../models/models')
 
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role}, 
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
+
 class UserController {
     async registration(req, res, next) {
         const {email, password, role} = req.body
@@ -15,16 +23,22 @@ class UserController {
         }
         const hashPassword = await bcrypt.hash(password, 5)
         const user = await User.create({email, role, password: hashPassword})
-        const token = jwt.sign(
-            {id: user.id, email: user.email, role}, 
-            process.env.SECRET_KEY,
-            {expiresIn: '24h'}
-        )
+        const token = generateJwt(user.id, user.email, user.role)
         return res.json({token})
     }
 
-    async login(req, res) {
-        
+    async login(req, res, next) {
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if (!user) {
+            return next(ApiError.internal('Пользователь не найден!'))
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.internal('Пользователь не найден!'))
+        }
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
     }
 
     async check(req, res, next) {
